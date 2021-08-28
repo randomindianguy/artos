96aed2c0085cd5da47a9a886e41e2154a07c7b2d
Sidharth Sundaram <sidharth.rama@gmail.com>
2021-08-28 05:42:42 +0530

    added new testimonial look

--- app.js
+++ app.js
@@ -11,4 +11,29 @@ for(i = 0; i < len; i++){
             panel.style.maxHeight = panel.scrollHeight + 'px';
         }
     })
+
+var btn = document.getElementsByClassName("test-btn");
+var slide = document.getElementById("slide");
+btn[0].oneClick = function(){
+    slide.style.transform = "translateX(0px)";
+    for(i = 0; i < 3; i++){
+    btn.classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn[1].oneClick = function(){
+    slide.style.transform = "translateX(-800px)";
+    for(i = 0; i < 3; i++){
+        btn.classList.remove("active");
+        }
+        this.classList.add("active");
+    }
+ btn[2].oneClick = function(){
+    slide.style.transform = "translateX(-1600px)";
+    for(i = 0; i < 3; i++){
+        btn.classList.remove("active");
+        }
+        this.classList.add("active");
+    }
+}
 }
